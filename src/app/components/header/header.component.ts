226cb8316ad13f0eea30152dfaaa351a1b566028
Gabs343 <gabriellopezp343@gmail.com>
2022-02-16 16:58:04 -0300

    creando un boton

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title:string = 'Task List';
-  
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleAddTask(){
+    console.log("click");
+  }
 }
