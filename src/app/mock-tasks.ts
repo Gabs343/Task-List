8d3cc0559d9f9a54216df62c9990937c6c4bf1ad
Gabs343 <gabriellopezp343@gmail.com>
2022-02-16 17:09:59 -0300

    agregando un mock de tareas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from "./Task";
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Tarea 1",
+        day: "Marzo 1 a las 13:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Tarea 2",
+        day: "Marzo 1 a las 13:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Tarea 3",
+        day: "Marzo 1 a las 13:00",
+        reminder: false
+    },
+]
